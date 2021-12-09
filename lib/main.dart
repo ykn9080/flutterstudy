653d90abd274bb4a93ad73532d30f041b1f7ed00
ykn9080 <ykn9080@empal.com>
2021-12-09 13:33:44 +0900

    lession16

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: NinjaCard(),
+    ));
 
-class MyApp extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
+        backgroundColor: Colors.grey,
+        elevation: 0,
       ),
-      body: Center(
-          child: IconButton(
-        onPressed: () {
-          print('you clicked');
-        },
-        icon: Icon(Icons.alternate_email),
-        color: Colors.amber,
-      )),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/girl1.jpg'),
+                  radius: 40,
+                ),
+              ),
+              Divider(
+                height: 90,
+                color: Colors.grey[800],
+              ),
+              Text('NAME',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  )),
+              SizedBox(height: 10),
+              Text('Chun-Li',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 30),
+              Text('CURRENT NINJA LEVEL',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2,
+                  )),
+              SizedBox(height: 10),
+              Text('8',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 30),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'chum.li@ther.co.uk',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('click'),
       ),
-    ));
+    );
   }
 }
